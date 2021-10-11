9e5a5575cde41df6bdc78d1cb87a1288566760ca
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2021-10-11 02:46:08 -0300

    feat: add 1.7_unicafe (step2) solution

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,6 +13,11 @@ const Button = ({text,onClick}) => {
 }
 
 const ResultLine = (props) => {
+  if (props.percent){
+    return (
+      <p>{props.text} {props.value} %</p>
+    )
+  }
   return (
     <p>{props.text} {props.value}</p>
   )
@@ -23,11 +28,13 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  const total = good+neutral+bad
 
   const goodPlusOne = () => setGood(good+1)
   const neutralPlusOne = () => setNeutral(neutral+1)
   const badPlusOne = () => setBad(bad+1)
-
+  
   return (
     <div>
       <Header text="give feedback"/>
@@ -35,9 +42,14 @@ const App = () => {
       <Button text="neutral" onClick={neutralPlusOne}/>
       <Button text="bad" onClick={badPlusOne}/>
       <Header text="statistics"/>
+
       <ResultLine text="good" value={good}/>
       <ResultLine text="neutral" value={neutral}/>
       <ResultLine text="bad" value={bad}/>
+      <ResultLine text="all" value={total}/>
+      <ResultLine text="average" value={(good-bad)/total}/>
+      <ResultLine text="positive" value={good/total*100} percent={true}/>
+
     </div>
   )
 }
