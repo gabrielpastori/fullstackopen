b8166d0ac44886e8542debfff2e2d3185eb096fc
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2021-10-11 02:34:16 -0300

    feat: add 1.6_unicafe (step1) solution

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const Header = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Button = ({text,onClick}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const ResultLine = (props) => {
+  return (
+    <p>{props.text} {props.value}</p>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodPlusOne = () => setGood(good+1)
+  const neutralPlusOne = () => setNeutral(neutral+1)
+  const badPlusOne = () => setBad(bad+1)
+
+  return (
+    <div>
+      <Header text="give feedback"/>
+      <Button text="good" onClick={goodPlusOne}/>
+      <Button text="neutral" onClick={neutralPlusOne}/>
+      <Button text="bad" onClick={badPlusOne}/>
+      <Header text="statistics"/>
+      <ResultLine text="good" value={good}/>
+      <ResultLine text="neutral" value={neutral}/>
+      <ResultLine text="bad" value={bad}/>
+    </div>
+  )
+}
+
+export default App
