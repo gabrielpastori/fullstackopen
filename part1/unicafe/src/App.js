eaa5ed84493bd5d01e392889d7d952c8289f470c
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2021-10-11 02:55:37 -0300

    feat: add 1.8_unicafe (step3) solution

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -23,18 +23,31 @@ const ResultLine = (props) => {
   )
 }
 
+const Statistics = ({good,neutral,bad}) => {
+  const total = good+neutral+bad
+  const average = (good-bad)/total
+  const positive = (good/total*100)
+  return (
+    <div>
+      <ResultLine text="good" value={good}/>
+      <ResultLine text="neutral" value={neutral}/>
+      <ResultLine text="bad" value={bad}/>
+      <ResultLine text="all" value={total}/>
+      <ResultLine text="average" value={average}/>
+      <ResultLine text="positive" value={positive} percent={true}/>
+    </div>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   
-  const total = good+neutral+bad
-
   const goodPlusOne = () => setGood(good+1)
   const neutralPlusOne = () => setNeutral(neutral+1)
   const badPlusOne = () => setBad(bad+1)
-  
+
   return (
     <div>
       <Header text="give feedback"/>
@@ -43,13 +56,7 @@ const App = () => {
       <Button text="bad" onClick={badPlusOne}/>
       <Header text="statistics"/>
 
-      <ResultLine text="good" value={good}/>
-      <ResultLine text="neutral" value={neutral}/>
-      <ResultLine text="bad" value={bad}/>
-      <ResultLine text="all" value={total}/>
-      <ResultLine text="average" value={(good-bad)/total}/>
-      <ResultLine text="positive" value={good/total*100} percent={true}/>
-
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
