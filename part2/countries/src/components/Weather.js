1dd578bfc85e0f68c3efbcf29a9733c8ec05adc4
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2021-10-27 15:19:10 -0300

    fix: replace country weather for capital weather

--- part2/countries/src/components/Weather.js
+++ part2/countries/src/components/Weather.js
@@ -3,12 +3,12 @@ import React, { useState, useEffect } from 'react';
 
 const Weather = ({ country }) => {
     const [weather, setWeather] = useState({});
-    const countryName = country.name.common;
+    const countryCapital = country.capital;
     const api_key = process.env.REACT_APP_API_KEY;
 
     useEffect(() => {
         axios
-            .get(`http://api.weatherstack.com/forecast?access_key=${api_key}&query=${countryName}&units=m`)
+            .get(`http://api.weatherstack.com/forecast?access_key=${api_key}&query=${countryCapital}&units=m`)
             .then((response) => {
                 setWeather(response.data.current);
             })
@@ -16,7 +16,7 @@ const Weather = ({ country }) => {
 
     return (
         <div>
-            <h2>Weather in {countryName}</h2>
+            <h2>Weather in {countryCapital}</h2>
             <p><b>temperature:</b> {weather.temperature} Celsius</p>
             <p><b>Wind:</b> {weather.wind_speed} kmh direction {weather.wind_dir}</p>
         </div>
