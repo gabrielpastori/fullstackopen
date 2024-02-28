c19811c1fdc3a511256a469992917be4ef6c197f
gabrielpastori <31520662+gabrielpastori@users.noreply.github.com>
2024-02-28 12:01:21 -0300

    add exercise 4.5

--- part4/blog/test/dummy.test.js
+++ part4/blog/test/dummy.test.js
@@ -51,3 +51,63 @@ describe('totalLikes', () => {
     assert.strictEqual(listHelper.totalLikes(twoBlogs), 12)
   })
 })
+
+describe('favoriteBlog', () => {
+  const noBlogs = []
+  const oneBlogs = [
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+  ]
+
+  const twoBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+  ]
+  
+  test('when list has no blog, equals to zero', () => {
+    assert.strictEqual(listHelper.favoriteBlog(noBlogs), 0)
+  })
+
+  test('when list has one blogs, equals the object with keys title, author and likes', () => {
+    assert.deepStrictEqual(listHelper.favoriteBlog(oneBlogs), 
+      {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12,
+      }
+    )
+  })
+
+  test('when list has two blogs, equals the object with most likes', () => {
+    assert.deepStrictEqual(listHelper.favoriteBlog(twoBlogs), 
+    {
+      title: "React patterns",
+      author: "Michael Chan",
+      likes: 7
+    }
+    )
+  })
+
+
+
+})
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,23 @@ const totalLikes = (blogs) => {
     return blogs.reduce((acc, curr) => acc + curr.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+     if (blogs.length === 0) return 0
+    const mostFavoritedBlog = blogs.reduce((prev, curr) => (prev && prev.likes > curr.likes) ? prev : curr)
+    const selectedBlog = {
+        title: mostFavoritedBlog.title,
+        author: mostFavoritedBlog.author,
+        likes: mostFavoritedBlog.likes,
+    }
+    return selectedBlog
+}
+
+const mostBlogs = (blogs) => {
+
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
